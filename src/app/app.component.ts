0b710b103ab15f96f9bfcae8b27ffe9fd6fc1367
Georgi Karapeev <g.karapeev@myxrobotics.com>
2024-02-12 00:38:53 +0200

    feat: add grid virtualization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { GridModule } from '@progress/kendo-angular-grid';
+import { GridDataResult, GridModule, PageChangeEvent } from '@progress/kendo-angular-grid';
 import { aggregatePairOverlapData } from '../core-functions/aggregate-pair-data';
 import { readCSVFile } from '../core-functions/read-csv-file';
 import { OverlapGridData, PointInTime, Result } from '../utils/types';
@@ -14,20 +14,51 @@ import { makeGridData } from './utils';
 	templateUrl: './app.component.html',
 	styleUrl: './app.component.scss',
 })
-export class AppComponent {
-	public gridData: OverlapGridData[] = [];
+export class AppComponent implements AfterViewInit {
+	public fullData: OverlapGridData[] = [];
+
+	public gridView: GridDataResult = {
+		data: [] as OverlapGridData[],
+		total: 0
+	};
+
+	public pageSize = 100;
+	public skip = 0;
+	public gridHeight!: number;
+
+	public loading = false;
+
+	private loadGridPage(): void {
+		this.gridView = {
+			data: this.fullData.slice(this.skip, this.skip + this.pageSize),
+			total: this.fullData.length,
+		};
+	}
+
+	public pageChange(event: PageChangeEvent): void {
+		this.skip = event.skip;
+		this.loadGridPage();
+	}
 
 	public async handleChange(e: Event) {
+		this.loading = true;
 		const result: Result = await readCSVFile(e);
-		
+
+		this.loading = false;
+
 		if (result.success) {
 			const points: PointInTime[] = result.payload!;
 			const pairs = aggregatePairOverlapData(points);
 			const data = makeGridData(pairs);
 
-			this.gridData = data;
+			this.fullData = data;
+			this.loadGridPage();
 		} else {
 			alert(result.message);
 		}
+	}
+
+	public ngAfterViewInit(): void {
+		this.gridHeight = window.innerHeight - 50;
 	}
 }
